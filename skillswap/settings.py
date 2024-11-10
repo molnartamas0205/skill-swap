8034fe856ee8f01c0731b59e4d60096eb336441b
molnartamas0205 <molnartamas0205@gmail.com>
2024-11-10 11:40:44 +0100

    Add tutoring to the list of installed apps in settings.py

--- skillswap/settings.py
+++ skillswap/settings.py
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Own apps
+    'tutoring',
+
+    # Third Party Apps
 ]
 
 MIDDLEWARE = [
