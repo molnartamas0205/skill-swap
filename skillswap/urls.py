ac7915db2cfe08aa5149ea5a6bc3332deddc4246
molnartamas0205 <molnartamas0205@gmail.com>
2024-11-10 18:33:45 +0100

    Add basic templates and views with url routing

--- skillswap/urls.py
+++ skillswap/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pages.views import home_view, about_view
 
 urlpatterns = [
+    path('home/', home_view, name='home'),
+    path('about/', about_view, name='about'),
     path('admin/', admin.site.urls),
 ]
