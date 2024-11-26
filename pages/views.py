a00465dbc507b70864ad47876abfd67c61e023bc
Tómán Péter <tomanpepe03@mailbox.unideb.hu>
2024-11-26 10:20:11 +0100

    Add basic search algorithm to check if a title contains the search term

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tutoring.models import TutoringService
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
@@ -8,5 +9,15 @@ def home_view(request, *args, **kwargs):
 def about_view(request, *args, **kwargs):
     return render(request, 'about.html', {})
 
-def search_venues_view(request):
-    return render(request, 'search_venues.html')
+def search_subjects_view(request):
+    if request.method == "POST":
+        searched = request.POST['searched']
+        subjects = TutoringService.objects.filter(title__contains=searched)
+
+        return render(request, 'search_subjects.html', 
+        {
+            'searched': searched,
+            'subjects': subjects
+        })
+    else:
+        return render(request, 'search_subjects.html', {})
