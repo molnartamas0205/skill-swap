864ff58f03bd6c2a2ea47748c2bbc25b52b54aad
Tómán Péter <tomanpepe03@mailbox.unideb.hu>
2024-11-27 08:31:40 +0100

    include category in search filtering

--- pages/views.py
+++ pages/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from tutoring.models import TutoringService
 from fuzzywuzzy import fuzz
-
-# Create your views here.
 def home_view(request, *args, **kwargs):
     return render(request, 'home.html', {})
 
@@ -17,8 +15,8 @@ def search_subjects_view(request):
         tutors = TutoringService.objects.all() 
         filtered_tutors = []
         for tutor in tutors:
-            if fuzz.partial_ratio(searched.lower(), tutor.title.lower()) > 70:  
-                filtered_tutors.append(tutor)
+            if fuzz.partial_ratio(searched.lower(), tutor.title.lower()) > 70 or fuzz.partial_ratio(searched.lower(), tutor.category.name.lower()) > 70:  
+             filtered_tutors.append(tutor)
 
         return render(request, 'search_subjects.html', {
             'searched': searched,
